729ef579caeee57f43e3006fecbdb9abb6e53362
8ctopus <hello@octopuslabs.io>
2024-04-05 09:13:17 +0400

    Cleanup architecture

--- src/PDOWrap.php
+++ src/PDOWrap.php
@@ -1,10 +1,8 @@
 <?php
 
-namespace Oct8pus\PDOFix;
+namespace Oct8pus\PDOWrap;
 
-use Exception;
 use PDO;
-use PDOStatement;
 
 class PDOWrap extends PDO
 {
@@ -17,59 +15,5 @@ class PDOWrap extends PDO
         }
 
         return new PDOStatementFix($result);
-    }
-}
-
-class PDOStatementFix extends PDOStatement
-{
-    private PDOStatement $statement;
-
-    public function __construct(PDOStatement $statement)
-    {
-        $this->statement = $statement;
-    }
-
-    public function execute(?array $params = null) : bool
-    {
-        if ($params) {
-            foreach ($params as $key => $value) {
-                $this->statement->bindValue($key, $value, $this->typeToParam($value));
-            }
-
-        }
-
-        return $this->statement->execute();
-    }
-
-    public function __call(string $method, array $args) : mixed
-    {
-        return $this->statement->{$method}($args);
-    }
-
-    /**
-     * Variable to PDO type
-     *
-     * @param  mixed  $value
-     *
-     * @return int PDO type
-     */
-    private function typeToParam(mixed $value) : int
-    {
-        switch ($type = gettype($value)) {
-            case 'boolean':
-                return PDO::PARAM_BOOL;
-
-            case 'integer':
-                return PDO::PARAM_INT;
-
-            case 'NULL':
-                return PDO::PARAM_NULL;
-
-            case 'string':
-                return PDO::PARAM_STR;
-
-            default:
-                throw new Exception("unsupported type - {$type}");
-        }
     }
 }
