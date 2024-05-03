1cf09bf80b251d31e331d4abc89bcaf020660d08
8ctopus <hello@octopuslabs.io>
2024-05-03 09:27:49 +0400

    Improve tests

---
+++ tests/PDODateTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oct8pus\PDOWrap;
+
+use DateTime;
+use PDO;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ *
+ * @covers \Oct8pus\PDOWrap\PDOStatementWrap
+ * @covers \Oct8pus\PDOWrap\PDODate
+ */
+class PDODateTest extends TestCase
+{
+    public function test() : void
+    {
+        $date = new PDODate('2021-10-08');
+
+        self::assertInstanceOf(DateTime::class, $date);
+        self::assertSame('2021-10-08', (string) $date);
+    }
+}
